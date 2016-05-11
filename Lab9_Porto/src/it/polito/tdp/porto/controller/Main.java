9082f23b8e2bbf32d27a2625ce5b6b3f89fdc9df
207988 <207988@studenti.polito.it>
2016-05-11 14:08:30 +0200

    *2.4.1

--- Lab9_Porto/src/it/polito/tdp/porto/controller/Main.java
+++ Lab9_Porto/src/it/polito/tdp/porto/controller/Main.java
@@ -1,5 +1,6 @@
 package it.polito.tdp.porto.controller;
 	
+import it.polito.tdp.porto.model.PortoModel;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -11,9 +12,12 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Porto.fxml"));
-			Scene scene = new Scene(root,400,400);
+			FXMLLoader loader =new FXMLLoader(getClass().getResource("Porto.fxml"));
+			BorderPane root=loader.load();
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			PortoController porto=loader.getController();
+			porto.setModel(new PortoModel());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
--- Lab9_Porto/src/it/polito/tdp/porto/controller/PortoController.java
+++ Lab9_Porto/src/it/polito/tdp/porto/controller/PortoController.java
@@ -1,5 +1,77 @@
 package it.polito.tdp.porto.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.porto.model.Autore;
+import it.polito.tdp.porto.model.PortoModel;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+
 public class PortoController {
+
+	PortoModel model;
 	
+	public void setModel(PortoModel porto){
+		this.model=porto;
+		model.caricaGrafo();
+		BoxAuth1.getItems().addAll(model.getAutori());
+		BoxAuth2.getItems().addAll(model.getAutori());
+		
+	}
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+    
+    @FXML
+    private ComboBox<Autore> BoxAuth1;
+
+    @FXML
+    private ComboBox<Autore> BoxAuth2;
+
+    @FXML
+    private Button btnCoautori;
+
+    @FXML
+    private Button btnCluster;
+
+    @FXML
+    private Button btnArticoli;
+
+    @FXML
+    private TextArea txtOutput;
+
+    @FXML
+    void doArticoli(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCluster(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCoautori(ActionEvent event) {
+    	String s="";
+    	for(Autore a:model.visualizzaCoautori(BoxAuth1.getValue()))
+    		s+=(a.toString()+"\n");
+    	txtOutput.setText(s);
+    }
+
+    @FXML
+    void initialize() {
+    	assert BoxAuth1 != null : "fx:id=\"BoxAuth1\" was not injected: check your FXML file 'Porto.fxml'.";
+        assert BoxAuth2 != null : "fx:id=\"BoxAuth2\" was not injected: check your FXML file 'Porto.fxml'.";
+        assert btnCoautori != null : "fx:id=\"btnCoautori\" was not injected: check your FXML file 'Porto.fxml'.";
+        assert btnCluster != null : "fx:id=\"btnCluster\" was not injected: check your FXML file 'Porto.fxml'.";
+        assert btnArticoli != null : "fx:id=\"btnArticoli\" was not injected: check your FXML file 'Porto.fxml'.";
+        assert txtOutput != null : "fx:id=\"txtOutput\" was not injected: check your FXML file 'Porto.fxml'.";
+
+    }
 }
